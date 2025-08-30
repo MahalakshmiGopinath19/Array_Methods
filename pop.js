9452f4d5c1b924289713a1044e3b8bc0fb7b5ec4
Maha <mahagmaha05@gmail.com>
2025-08-30 22:27:41 +0530

    Created pop.js

---
+++ pop.js
@@ -0,0 +1,11 @@
+//1.
+var fruits = ["orange", "apple", "grapes", "mango", "watermelon", "guva", "strawberry"];
+console.log(fruits);
+var new_sets = fruits.pop();
+console.log(fruits);
+
+//2.
+var array = ["one", "two", "three", "four", "five", "six"];
+console.log(array);
+array.pop();
+console.log(array);
