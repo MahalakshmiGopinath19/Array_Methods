e65e6d66667d74544ff6f3a2e323a120a1278756
Maha <mahagmaha05@gmail.com>
2025-08-30 22:26:28 +0530

    Created slice.js

---
+++ slice.js
@@ -0,0 +1,18 @@
+//1.
+var array = ["birds", "trees", "fish", "land", "sea", "boats"];
+console.log(array.slice(2, 5));
+console.log(array.slice(1, 3));
+
+//2.
+var array = ["fire", "water", "ice", "steam", "sand", "grass"];
+console.log(array.slice(0, 4));
+console.log(array.slice(3));
+console.log(array.slice(5));
+console.log(array.slice(-1));
+console.log(array.slice(-2));
+console.log(array.slice(-5, -2));
+
+//3.
+var string = "Now, this is a string to be slicing";
+console.log(string.slice(3, 8));
+console.log(string.slice(6, 19));
