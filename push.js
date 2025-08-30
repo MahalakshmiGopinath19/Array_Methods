ec9953beaab49f89ad548e97a330fd00558eb239
Maha <mahagmaha05@gmail.com>
2025-08-30 22:27:08 +0530

    Created push.js

---
+++ push.js
@@ -0,0 +1,17 @@
+//1.
+var array = ["one", "two", "three", "four", "five"];
+array.push("six");
+console.log(array);
+
+//2.
+var fruits = ["apple", "banana", "orange", "grapes", "mango", "watermelon"];
+fruits.push("guva");
+console.log(fruits);
+
+//3.
+var fruits = ["apple", "banana", "orange", "grapes", "mango", "watermelon"];
+console.log(fruits);
+var added_Fruits = fruits.push("guva");
+console.log(added_Fruits);
+console.log(fruits);
+
