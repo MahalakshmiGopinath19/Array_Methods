b833f9ae49a0e05ab387a757e0082b276aafb8f1
Maha <mahagmaha05@gmail.com>
2025-08-30 22:28:44 +0530

    Created unshift.js

---
+++ unshift.js
@@ -0,0 +1,9 @@
+//1.
+var array = ["one", "two", "three", "four", "five", "six"];
+array.unshift("zero");
+console.log(array);
+
+//2.
+var arr = ["water", "land"];
+arr.unshift("air");
+console.log(arr);
