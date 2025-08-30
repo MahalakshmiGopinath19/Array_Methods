7fd672363d346657a71d5cff4553f187f037592d
Maha <mahagmaha05@gmail.com>
2025-08-30 22:28:14 +0530

    Created shift.js

---
+++ shift.js
@@ -0,0 +1,11 @@
+//1.
+var array = ["one", "two", "three", "four", "five", "six"];
+console.log(array.shift());
+
+//2.
+var array = ["birds", "trees", "fish", "land", "sea", "boats"];
+console.log(array.shift());
+
+//3.
+var array = ["fire", "water", "ice", "steam", "sand", "grass"];
+console.log(array.shift());
